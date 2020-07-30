ac9c548b3d8645934f6fb6316b5a0c761eace543
yc-How-python <66516657+yc-How-python@users.noreply.github.com>
2020-07-30 11:53:33 +0800

    Update Fundcathcer_newversion.py

--- Fundcathcer_newversion.py
+++ Fundcathcer_newversion.py
@@ -1,30 +1,103 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import re
-from PyQt5.QtGui import  QColor, QBrush
+from re import search
+from PyQt5.QtGui import  QColor,QBrush,QDesktopServices
 from PyQt5 import QtWidgets,QtCore
 from PyQt5 import uic
 from time import sleep
 from numpy import array
+
 from sys import argv,exit
 
 class ThreadTable(QtCore.QThread):
-    update_date = QtCore.pyqtSignal()
-
-    def __init__(self,parent=None,*args,**kwargs):
+    signalrefresh = QtCore.pyqtSignal(tuple)
+    def __init__(self,window,codelist,moneylist,parent=None,*args,**kwargs):
         super(ThreadTable, self).__init__(parent,*args,**kwargs)
+        self.window=window
+        self.codelist=codelist
+        self.moneylist=moneylist
 
+    def loadling(self, code):
+        #print(code)
+        page = urlopen('http://fund.eastmoney.com/' + str(code) + '.html?spm=search').read().decode(
+            'utf-8')
+        soup = BeautifulSoup(page, features='lxml')
+        return soup
+
+    def name_extract(self, bs):
+        bs_name = bs.find_all('div', {'style': "float: left"})  # div 中寻找 style="float: left"
+        name = search(r'>.*?<', str(bs_name))
+        return name[0].split('>')[1].split('<')[0]
+
+    def estimated_value_extract(self, bs):
+        bs_value = self.bs.find_all('span', {'id': "gz_gsz"})
+        # <span class="ui-font-large ui-color-red ui-num" id="gz_gsz">1.0626</span>
+        # span 中寻找 id = gz_gsz
+        value = search(r'>.*?</span>', str(bs_value))
+        return value[0].split('>')[1].split('<')[0]
+
+    def delta_rate_extract(self, bs):
+        bs_rate = bs.find_all('span', {'id': "gz_gszzl"})
+        # [<span class="ui-font-middle ui-color-red ui-num" id="gz_gszzl">+1.70%</span>]
+        rate = float(search(r'>.*?</span>', str(bs_rate))[0].split('>')[1].split('<')[0][:-1])
+        return rate
     def run(self):
 
         while True:
-            self.update_date.emit()  # 发射信号
+            self.table_data_update()
+
             sleep(30)#每隔三秒钟发射一次
+    def table_data_update(self):
+
+
+        row = 0
+
+        profitlist = []
+        for each in self.codelist:
+            codeItem = QtWidgets.QTableWidgetItem(str(each))
+
+            soup = self.loadling(each.strip(' '))
+
+
+
+            nameItem = QtWidgets.QTableWidgetItem(str(self.name_extract(soup)))
+
+
+            money = self.moneylist[row]
+            print(money)
+            moneyItem = QtWidgets.QTableWidgetItem(str(money))
+            rate = self.delta_rate_extract(soup)
+            rateItem = QtWidgets.QTableWidgetItem(str(rate))
+
+
+            profit = round(rate * 0.01 * float(money), 2)
+            profitItem = QtWidgets.QTableWidgetItem(str(profit))
+
+            self.signalrefresh.emit((row, 0, codeItem))
+            self.signalrefresh.emit((row, 1, nameItem))
+            self.signalrefresh.emit((row, 2, moneyItem))
+            self.signalrefresh.emit((row, 3, rateItem))
+            self.signalrefresh.emit((row, 4, profitItem))
+
+            row += 1
+            profitlist.append(profit)
+        totalmoney = 0
+        for each in self.moneylist:
+            totalmoney += float(each)
+        newItem = QtWidgets.QTableWidgetItem(str(totalmoney))
+
+        self.signalrefresh.emit((row, 2, newItem))
+
+
+        profitarray = array(profitlist)
+        profit_total = profitarray.sum()
+        print("盈利更新：",profit_total)
+
+        newItem = QtWidgets.QTableWidgetItem(str(profit_total))
+        self.signalrefresh.emit((row, 4, newItem))
+
 
 class Fund_UI():
-
-
-
-
 
     def __init__(self):
         #加载ui文件
@@ -36,20 +109,14 @@ class Fund_UI():
         codelist = []
         moneylist = []
         row=0
+
         for eachline in funddata[1:]:
             if eachline=='':
                 continue
             code,money=eachline.strip().split('\t')
             codelist.append(code)
-
-
             moneylist.append(money)
-
-
             row+=1
-
-
-
         self.window.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint|QtCore.Qt.CustomizeWindowHint)
 
         self.loc()
@@ -70,11 +137,26 @@ class Fund_UI():
             self.window.Table2.setItem(order, 1, newItem)
 
 
-        self.table_thread = ThreadTable()
+        self.table_thread = ThreadTable(self.window,self.codelist,self.moneylist)
         self.table_thread.start()
-        self.table_thread.update_date.connect(self.table_data_update)
+
         self.window.sumbit.clicked.connect(self.update_setup)
         self.window.setting.clicked.connect(self.table_resize)
+        self.table_thread.signalrefresh.connect(self.ui_refresh)
+        self.window.pushButton.clicked.connect(self.intro)
+    def intro(self):
+        QDesktopServices.openUrl(QtCore.QUrl('https://github.com/yc-How-python/Fundcatcher/blob/master/README.md'))
+    def ui_refresh(self,tuplevar):
+        i,j,var=tuplevar
+        self.window.Table.setItem(i,j,var)
+        if j==0:
+            self.window.Table.item(i, 0).setForeground(QBrush(QColor(255, 0, 0)))
+        elif j==4:
+            if float(var.text()) >= 0:
+                self.window.Table.item(i, 4).setForeground(QBrush(QColor(255, 0, 0)))
+            else:
+                self.window.Table.item(i, 4).setForeground(QBrush(QColor(100, 255, 140)))
+
     def table_resize(self):
         newrow=self.window.numberoffunds.value()
         self.window.Table2.setRowCount(newrow)
@@ -102,98 +184,10 @@ class Fund_UI():
             dataf.write(code+'\t'+money+'\n')
         print(self.codelist)
         print(self.moneylist)
+
         dataf.close()
-
-
-
-
-    def loadling(self, code):
-        #print(code)
-        page = urlopen('http://fund.eastmoney.com/' + str(code) + '.html').read().decode(
-            'utf-8')
-        soup = BeautifulSoup(page, features='lxml')
-        return soup
-
-    def name_extract(self, bs):
-        bs_name = bs.find_all('div', {'style': "float: left"})  # div 中寻找 style="float: left"
-        name = re.search(r'>.*?<', str(bs_name))
-        return name[0].split('>')[1].split('<')[0]
-
-    def estimated_value_extract(self, bs):
-        bs_value = self.bs.find_all('span', {'id': "gz_gsz"})
-        # <span class="ui-font-large ui-color-red ui-num" id="gz_gsz">1.0626</span>
-        # span 中寻找 id = gz_gsz
-        value = re.search(r'>.*?</span>', str(bs_value))
-        return value[0].split('>')[1].split('<')[0]
-
-    def delta_rate_extract(self, bs):
-        bs_rate = bs.find_all('span', {'id': "gz_gszzl"})
-        # [<span class="ui-font-middle ui-color-red ui-num" id="gz_gszzl">+1.70%</span>]
-        rate = float(re.search(r'>.*?</span>', str(bs_rate))[0].split('>')[1].split('<')[0][:-1])
-        return rate
-    def table_data_update(self):
-
-
-        row = 0
-        col = 0
-        profitlist = []
-        for each in self.codelist:
-            newItem = QtWidgets.QTableWidgetItem(str(each))
-            self.window.Table.setItem(row, 0, newItem)
-            soup = self.loadling(each.strip(' '))
-
-            newItem = QtWidgets.QTableWidgetItem(str(self.name_extract(soup)))
-            self.window.Table.setItem(row, 1, newItem)
-
-            money = self.moneylist[row]
-            newItem = QtWidgets.QTableWidgetItem(str(money))
-            self.window.Table.setItem(row, 2, newItem)
-
-            self.window.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-
-            self.window.Table.item(row, 0).setForeground(QBrush(QColor(255, 0, 0)))
-
-            rate = self.delta_rate_extract(soup)
-            newItem = QtWidgets.QTableWidgetItem(str(rate))
-            self.window.Table.setItem(row, 3, newItem)
-            profit = round(rate * 0.01 * float(money), 2)
-            newItem = QtWidgets.QTableWidgetItem(str(profit))
-            self.window.Table.setItem(row, 4, newItem)
-            if profit >= 0:
-                self.window.Table.item(row, 4).setForeground(QBrush(QColor(255, 0, 0)))
-            else:
-                self.window.Table.item(row, 4).setForeground(QBrush(QColor(100, 255, 140)))
-            row += 1
-            profitlist.append(profit)
-
-
-
-
-        newItem = QtWidgets.QTableWidgetItem("统计")
-        self.window.Table.setItem(row, 0, newItem)
-        newItem = QtWidgets.QTableWidgetItem(" ")
-        self.window.Table.setItem(row, 1, newItem)
-
-        totalmoney = 0
-        for each in self.moneylist:
-            totalmoney += float(each)
-        newItem = QtWidgets.QTableWidgetItem(str(totalmoney))
-        self.window.Table.setItem(row, 2, newItem)
-
-        newItem = QtWidgets.QTableWidgetItem(str(' '))
-        self.window.Table.setItem(row, 3, newItem)
-
-        profitarray = array(profitlist)
-        profit_total = profitarray.sum()
-        print(profit_total, self.totalrow)
-
-        newItem = QtWidgets.QTableWidgetItem(str(profit_total))
-        self.window.Table.setItem(row, 4, newItem)
-
-        if profit_total >= 0:
-            self.window.Table.item(row, 4).setForeground(QBrush(QColor(255, 0, 0)))
-        else:
-            self.window.Table.item(row, 4).setForeground(QBrush(QColor(100, 255, 140)))
+        self.table_thread.codelist=self.codelist
+        self.table_thread.moneylist=self.moneylist
 
     def loc(self):
        self.window.move((self.screenRect.width()-self.window.width()), (self.screenRect.height()-self.window.height()-50))  # 利用move函数窗口
